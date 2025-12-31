08305fb057046d621cbeb071447bcf939acc7594
andreasthuis <andreasdeborger27@gmail.com>
2025-12-31 19:14:58 +0100

    initial commit

---
+++ webpack.renderer.config.js
@@ -0,0 +1,22 @@
+const rules = require('./webpack.rules');
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        type: "javascript/auto",
+      },
+    ],
+  },
+}

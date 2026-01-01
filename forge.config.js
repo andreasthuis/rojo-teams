8531fbc6085240f3d52d12b1ad2251eeac7609eb
andreasthuis <andreasdeborger27@gmail.com>
2026-01-01 11:19:53 +0100

    added rounded corners (took 3h)

--- forge.config.js
+++ forge.config.js
@@ -12,15 +12,7 @@ module.exports = {
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
       name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
       config: {},
     },
   ],
